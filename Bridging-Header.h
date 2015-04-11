28209cc2df94f5fdd748e56760005c392b7b7754
James Yanyuk <jyanyuk@gmail.com>
2015-04-11 14:02:39 -0400

    Setting up project structure

---
+++ Bridging-Header.h
@@ -0,0 +1,12 @@
+//
+//  Bridging-Header.h
+//  Placemark
+//
+//  Created by James Yanyuk on 4/11/15.
+//  Copyright (c) 2015 placemark. All rights reserved.
+//
+
+#ifndef Placemark_Bridging_Header_h
+#define Placemark_Bridging_Header_h
+
+#endif
