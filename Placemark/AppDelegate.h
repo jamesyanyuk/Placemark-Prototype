3f8ffca7f90a8a33daa4e426d0419fc5df0d5ccb
James Yanyuk <jyanyuk@gmail.com>
2015-04-11 16:36:12 -0400

    Base complete

---
+++ Placemark/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Placemark
+//
+//  Created by James Yanyuk on 4/11/15.
+//  Copyright (c) 2015 placemark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Placemark/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  Placemark
+//
+//  Created by James Yanyuk on 4/11/15.
+//  Copyright (c) 2015 placemark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MetaioSDK/MetaioSDKViewController.h>
+
+@interface ViewController : MetaioSDKViewController
+
+
+@end
+
